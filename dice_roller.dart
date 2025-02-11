d819ac0eec8969eec1bb62e7a31f60cebd0df11e
Dikadwii <kikidos3@gmail.com>
2025-02-11 08:24:52 +0700

    versi baru

--- dice_roller.dart
+++ dice_roller.dart
@@ -14,17 +14,11 @@ class DiceRoller extends StatefulWidget {
 
 class _DiceRollerState extends State<DiceRoller> {
   var currentDiceRoll = 2;
-   var currentDiceRoll2 = 4;
-   var currentDiceRoll3 = 6;
-    var currentDiceRoll4 = 3;
-  
 
   void rollDice() {
     setState(() {
       currentDiceRoll = randomizer.nextInt(6) + 1;
-       currentDiceRoll2 = randomizer.nextInt(6) + 1;
-        currentDiceRoll3 = randomizer.nextInt(6) + 1;
-         currentDiceRoll4 = randomizer.nextInt(6) + 1;
+      
     });
   }
   
@@ -32,25 +26,14 @@ class _DiceRollerState extends State<DiceRoller> {
   @override
   build(context) {
     return Row(
-      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       
       children: [
         Image.asset(
           'asset/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
-        Image.asset(
-          'asset/images/dice-$currentDiceRoll2.png',
-          width: 200,
-        ),
-        Image.asset(
-          'asset/images/dice-$currentDiceRoll3.png',
-          width: 200,
-        ),
-         Image.asset(
-          'asset/images/dice-$currentDiceRoll4.png',
-          width: 200,
-        ),
+       
         TextButton(
           onPressed: rollDice,
           style: TextButton.styleFrom(
@@ -60,7 +43,7 @@ class _DiceRollerState extends State<DiceRoller> {
             foregroundColor: const Color.fromARGB(255, 0, 0, 0),
             textStyle: TextStyle(fontSize: 28) ,
           ),
-          child: const Text('lacak kb!'),
+          child: const Text('acak!'),
         )
       ],
     );
